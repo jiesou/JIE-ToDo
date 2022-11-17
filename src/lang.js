36d0aacfbfd6b6294897849272fe2ed7653666c1
jiesou <jiechushou@gmail.com>
2022-11-17 21:15:06 +0800

    添加多语言映射表

--- src/lang.js
+++ src/lang.js
@@ -1,5 +1,14 @@
-let _language = new URL(location.href).searchParams.get("language") || navigator.language;
-fetch(`/string/${_language}.json`).then(async (res) => {
+let _language = (new URL(location.href).searchParams.get("language") || navigator.language)
+    .toLowerCase();
+let _langMap = {
+	en: 'en-us',
+	zh: 'zh-hans',
+	'zh-cn': 'zh-hans',
+	'zh-hk': 'zh-hant',
+	'zh-tw': 'zh-hant',
+	'zh-sg': 'zh-hans',
+}
+fetch(`/string/${_langMap[_language] || 'en-us'}.json`).then(async (res) => {
   let lang = await res.json();
   document.title = lang.appname || 'JIE-ToDo';
   for(let key in lang){
